459a7daf19461ee744c0efebf770d58d2cbbf027
yukuima <58396683@qq.com>
2019-06-05 21:52:00 +0800

    commit

---
+++ src/test/java/com/myk/learning/validator/ValidateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myk.learning.validator;
+
+import org.junit.Test;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class ValidateApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
